0f0e3dfe972763adcb628eef786574dc2f5a2d51
lingyuanhai <924005478@qq.com>
2017-07-08 23:33:37 +0800

    Update helloworld.java
    测试修改行不行。

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,7 @@
 public class helloworld{
+   public helloworld(){
+    System.out.println("世界如此美妙，而我却如此烦躁。这样不好。不好");
+   }
   public static void main(String[]agrs){
     System.out.println("世界你好");
   }
